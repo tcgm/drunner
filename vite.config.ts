0b61b0ad07dca9cc4ba763d88d0f054014cb8063
TCGM <thecrazygamemaster@gmail.com>
2026-01-29 06:36:06 -0800

    refactor(config): set BASE_URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.BASE_URL || '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
