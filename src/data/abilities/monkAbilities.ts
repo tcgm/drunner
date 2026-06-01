73d90054ebe60b75c43db3978848e29cacd77d19
TCGM <thecrazygamemaster@gmail.com>
2026-06-01 07:33:00 -0800

    feat: add Monk class with abilities; Bellend is now a monk

---
+++ src/data/abilities/monkAbilities.ts
@@ -0,0 +1,16 @@
+/**
+ * Monk Abilities
+ */
+export { IRON_FIST } from './ironFist'
+export { INNER_FOCUS } from './innerFocus'
+export { KI_STRIKE } from './kiStrike'
+
+import { IRON_FIST } from './ironFist'
+import { INNER_FOCUS } from './innerFocus'
+import { KI_STRIKE } from './kiStrike'
+
+export const MONK_ABILITIES = [
+    IRON_FIST,
+    INNER_FOCUS,
+    KI_STRIKE,
+]
